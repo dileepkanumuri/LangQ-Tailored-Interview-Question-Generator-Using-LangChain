c197cb4e93e298582bc29f975176067f077eab5e
dileepkanumuri <dileepkanumuri1998@gmail.com>
2024-11-09 16:07:21 -0500

    Updated Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "LangQ - Generative AI Interview Question Generator project",
+    version= '0.0.0',
+    author="Dileep Kanumuri, Guru Saranya Gopireddy, Satwik Dikkala",
+    author_email="dileepkanumuri.adm@gmail.com, saranyagopireddy2801@gmail.com, satwikds@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
